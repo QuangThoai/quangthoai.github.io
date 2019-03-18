3974c9936d71a73c0f94051a1f756fb3e6913f92
quangthoai <tranquangthoai19970730@gmail.com>
2019-03-18 15:58:01 +0700

    Upload framework

--- PHP/Week_5/FrameWork/views/user/js/action.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  var btnShow = $('#js-btn-show');
-  var listUsers = $('#js-users');
-
-  btnShow.click(function() {
-    listUsers.css('display', 'block');
-  });
-});
